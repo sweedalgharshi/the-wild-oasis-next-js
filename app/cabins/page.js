b34043be794b7117d48839938a6f4296417c0a81
sweedalgharshi <sweedalgracius@gmail.com>
2024-10-08 14:56:13 +0530

    Added layout comonent along with logo component and website logo image

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -3,7 +3,6 @@ import Navigation from "../components/Navigation";
 export default function cabins() {
   return (
     <div>
-      <Navigation />
       <h1>cabins page</h1>
     </div>
   );
--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,21 @@
-export default function RootLayout() {
+import Logo from "./components/Logo";
+import Navigation from "./components/Navigation";
+
+export const metadata = {
+  title: "The Wild Oasis",
+};
+
+export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body></body>
+      <body>
+        <header>
+          <Logo />
+          <Navigation />
+        </header>
+        <main>{children}</main>
+        <footer>Copyright by The Wild Oasis.</footer>
+      </body>
     </html>
   );
 }
