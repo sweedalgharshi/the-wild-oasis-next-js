97c21ae3eeab5f5f2215b5df28e7a6f32a319e8a
sweedalgharshi <sweedalgracius@gmail.com>
2024-10-21 12:02:28 +0530

    Improving the navigation and root layout

--- app/_components/Logo.js
+++ app/_components/Logo.js
@@ -1,9 +1,13 @@
+import Image from "next/image";
 import Link from "next/link";
+
+import logo from "@/public/logo.png";
 
 function Logo() {
   return (
     <Link href="/" className="flex items-center gap-4 z-10">
-      <img src="/logo.png" alt="The Wild Oasis logo" height="60" width="60" />
+      {/* <Image src="/logo.png" alt="The Wild Oasis logo" height="60" width="60" /> */}
+      <Image src={logo} alt="The Wild Oasis logo" height="60" width="60" quality={100} />
       <span className="text-xl font-semibold text-primary-100">The Wild Oasis</span>
     </Link>
   );
--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,12 @@
-import Logo from "@/app/_components/Logo";
-import Navigation from "@/app/_components/Navigation";
-
 import "@/app/_styles/globals.css";
+
+import { Josefin_Sans } from "next/font/google";
+import Header from "./_components/Header";
+
+const josefin = Josefin_Sans({
+  subsets: ["latin"],
+  display: "swap",
+});
 
 export const metadata = {
   // title: "The Wild Oasis",
@@ -16,13 +21,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-primary-950 text-primary-100 min-h-screen">
-        <header>
-          <Logo />
-          <Navigation />
-        </header>
-        <main>{children}</main>
-        <footer>Copyright by The Wild Oasis.</footer>
+      <body
+        className={`bg-primary-950 text-primary-100 antialiased min-h-screen flex flex-col ${josefin.className}`}
+      >
+        <Header />
+
+        <div className="flex-1 px-8 py-12">
+          <main className="max-w-7xl mx-auto">{children}</main>
+        </div>
       </body>
     </html>
   );
