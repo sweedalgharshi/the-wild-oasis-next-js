008bfaeb2847466ab694fd79e38ad8862db35548
sweedalgharshi <sweedalgracius@gmail.com>
2024-12-02 15:26:53 +0530

    building the guest reservation page

--- app/_lib/data-service.js
+++ app/_lib/data-service.js
@@ -103,6 +103,42 @@ export async function getGuest(email) {
   return data;
 }
 
+// export async function getBookings(guestId) {
+//   const { data, error, count } = await supabase
+//     .from("bookings")
+//     // We actually also need data on the cabins as well. But let's ONLY take the data that we actually need, in order to reduce downloaded data.
+//     .select(
+//       "id, created_at, startDate, endDate, numNights, numGuests, totalPrice, guestId, cabinId, cabins(name, image)"
+//     )
+//     .eq("guestId", guestId)
+//     .order("startDate");
+
+//   if (error) {
+//     console.log(error);
+//     throw new Error("Bookings could not be loaded");
+//   }
+
+//   return data;
+// }
+
+export async function getBookings(guestId) {
+  const { data, error, count } = await supabase
+    .from("bookings")
+    // We actually also need data on the cabins as well. But let's ONLY take the data that we actually need, in order to reduce downloaded data.
+    .select(
+      "id, created_at, startDate, endDate, numNights, numGuests, totalPrice, guestId, cabinId, cabins(name, image)"
+    )
+    .eq("guestId", guestId)
+    .order("startDate");
+
+  if (error) {
+    console.error(error);
+    throw new Error("Bookings could not get loaded");
+  }
+
+  return data;
+}
+
 // /////////////
 // // CREATE
 
